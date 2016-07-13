cb1382c3afefad412225d354176640639a087405
Andreas Tasch <andy.tasch@gmail.com>
2016-07-13 18:31:36 +0200

    Reformat code.

--- unzipper.php
+++ unzipper.php
@@ -8,7 +8,7 @@
  * @author  Andreas Tasch, at[tec], attec.at
  * @license GNU GPL v3
  * @package attec.toolbox
- * @version 0.0.2 Alpha
+ * @version 0.0.3 Beta
  */
 
 $timestart = microtime(TRUE);
@@ -36,7 +36,7 @@ class Unzipper {
       }
       closedir($dh);
 
-      if(!empty($this->zipfiles)) {
+      if (!empty($this->zipfiles)) {
         self::$status = '.zip or .gz files found, ready for extraction';
       }
       else {
@@ -50,16 +50,17 @@ class Unzipper {
     $destination = isset($_POST['extpath']) ? strip_tags($_POST['extpath']) : '';
     //allow only local existing archives to extract
     if ($input !== '') {
-      if(empty($destination)) {
-               $extpath = $this->localdir;
-        } else {
-            $extpath = $this->localdir . '/'. $destination;
-            if(!is_dir($extpath)) {
-              mkdir($extpath);
-            }
+      if (empty($destination)) {
+        $extpath = $this->localdir;
+      }
+      else {
+        $extpath = $this->localdir . '/' . $destination;
+        if (!is_dir($extpath)) {
+          mkdir($extpath);
         }
+      }
       if (in_array($input, $this->zipfiles)) {
-        self::extract($input,$extpath);
+        self::extract($input, $extpath);
       }
     }
   }
@@ -85,7 +86,7 @@ class Unzipper {
    */
   public static function extractZipArchive($archive, $destination) {
     // Check if webserver supports unzipping.
-    if(!class_exists('ZipArchive')) {
+    if (!class_exists('ZipArchive')) {
       self::$status = '<span style="color:red; font-weight:bold;font-size:120%;">Error: Your PHP version does not support unzip functionality.</span>';
       return;
     }
@@ -95,7 +96,7 @@ class Unzipper {
     // Check if archive is readable.
     if ($zip->open($archive) === TRUE) {
       // Check if destination is writable
-      if(is_writeable($destination . '/')) {
+      if (is_writeable($destination . '/')) {
         $zip->extractTo($destination);
         $zip->close();
         self::$status = '<span style="color:green; font-weight:bold;font-size:120%;">Files unzipped successfully</span>';
@@ -117,7 +118,7 @@ class Unzipper {
    */
   public static function extractGzipFile($archive, $destination) {
     // Check if zlib is enabled
-    if(!function_exists('gzopen')) {
+    if (!function_exists('gzopen')) {
       self::$status = '<span style="color:red; font-weight:bold;font-size:120%;">Error: Your PHP has no zlib support enabled.</span>';
       return;
     }
@@ -133,7 +134,7 @@ class Unzipper {
     fclose($file);
 
     // Check if file was extracted.
-    if(file_exists($destination . '/' . $filename)) {
+    if (file_exists($destination . '/' . $filename)) {
       self::$status = '<span style="color:green; font-weight:bold;font-size:120%;">File unzipped successfully.</span>';
     }
     else {
@@ -150,92 +151,101 @@ class Unzipper {
   <title>File Unzipper</title>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <style type="text/css">
-    <!-- body {
-    font-family: Arial, serif;
-    line-height: 150%;
+    <!--
+    body {
+      font-family: Arial, sans-serif;
+      line-height: 150%;
     }
+
     fieldset {
-    border: 0px solid #000;
+      border: 0px solid #000;
     }
+
     .select {
-    padding: 5px;
-    font-size: 110%;
+      padding: 5px;
+      font-size: 110%;
     }
+
     .status {
-    margin-top: 20px;
-    padding: 5px;
-    font-size: 80%;
-    background: #EEE;
-    border: 1px dotted #DDD;
+      margin-top: 20px;
+      padding: 5px;
+      font-size: 80%;
+      background: #EEE;
+      border: 1px dotted #DDD;
     }
+
     .form-field {
-    border: 2px solid black;
-    -webkit-border-radius: 10px;
-    -moz-border-radius: 10px;
-    border-radius: 10px;
-    -webkit-box-shadow: rgba(255, 255, 255, 0.4) 0 0px 0, inset rgba(000, 000, 000, 0.7) 0 0px 0px;
-    -moz-box-shadow: rgba(255, 255, 255, 0.4) 0 0px 0, inset rgba(000, 000, 000, 0.7) 0 0px 0px;
-    box-shadow: rgba(255, 255, 255, 0.4) 0 0px 0, inset rgba(000, 000, 000, 0.7) 0 0px 0px;
-    padding: 8px;
-    margin-bottom: 20px;
-    width: 280px;
+      border: 2px solid black;
+      -webkit-border-radius: 10px;
+      -moz-border-radius: 10px;
+      border-radius: 10px;
+      -webkit-box-shadow: rgba(255, 255, 255, 0.4) 0 0px 0, inset rgba(000, 000, 000, 0.7) 0 0px 0px;
+      -moz-box-shadow: rgba(255, 255, 255, 0.4) 0 0px 0, inset rgba(000, 000, 000, 0.7) 0 0px 0px;
+      box-shadow: rgba(255, 255, 255, 0.4) 0 0px 0, inset rgba(000, 000, 000, 0.7) 0 0px 0px;
+      padding: 8px;
+      margin-bottom: 20px;
+      width: 280px;
     }
+
     .submit {
-    -moz-box-shadow: inset 0px 1px 0px 0px #bbdaf7;
-    -webkit-box-shadow: inset 0px 1px 0px 0px #bbdaf7;
-    box-shadow: inset 0px 1px 0px 0px #bbdaf7;
-    background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #79bbff), color-stop(1, #378de5));
-    background: -moz-linear-gradient(center top, #79bbff 5%, #378de5 100%);
-    filter: progid: DXImageTransform.Microsoft.gradient(startColorstr='#79bbff', endColorstr='#378de5');
-    background-color: #79bbff;
-    -moz-border-radius: 4px;
-    -webkit-border-radius: 4px;
-    border-radius: 4px;
-    border: 1px solid #84bbf3;
-    display: inline-block;
-    color: #ffffff;
-    font-family: arial;
-    font-size: 15px;
-    font-weight: bold;
-    padding: 10px 24px;
-    text-decoration: none;
-    text-shadow: 1px 1px 0px #528ecc;
+      -moz-box-shadow: inset 0px 1px 0px 0px #bbdaf7;
+      -webkit-box-shadow: inset 0px 1px 0px 0px #bbdaf7;
+      box-shadow: inset 0px 1px 0px 0px #bbdaf7;
+      background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #79bbff), color-stop(1, #378de5));
+      background: -moz-linear-gradient(center top, #79bbff 5%, #378de5 100%);
+      filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#79bbff', endColorstr='#378de5');
+      background-color: #79bbff;
+      -moz-border-radius: 4px;
+      -webkit-border-radius: 4px;
+      border-radius: 4px;
+      border: 1px solid #84bbf3;
+      display: inline-block;
+      color: #ffffff;
+      font-family: arial;
+      font-size: 15px;
+      font-weight: bold;
+      padding: 10px 24px;
+      text-decoration: none;
+      text-shadow: 1px 1px 0px #528ecc;
     }
+
     .submit:hover {
-    background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #378de5), color-stop(1, #79bbff));
-    background: -moz-linear-gradient(center top, #378de5 5%, #79bbff 100%);
-    filter: progid: DXImageTransform.Microsoft.gradient(startColorstr='#378de5', endColorstr='#79bbff');
-    background-color: #378de5;
+      background: -webkit-gradient(linear, left top, left bottom, color-stop(0.05, #378de5), color-stop(1, #79bbff));
+      background: -moz-linear-gradient(center top, #378de5 5%, #79bbff 100%);
+      filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#378de5', endColorstr='#79bbff');
+      background-color: #378de5;
     }
+
     .submit:active {
-    position: relative;
-    top: 1px;
+      position: relative;
+      top: 1px;
     }
-    /* This imageless css button was generated by CSSButtonGenerator.com */
+    -->
   </style>
 </head>
 <body>
-  <h1>Archive Unzipper</h1>
-  <p>Select .zip archive or .gz file you want to extract:</p>
-  <form action="" method="POST">
-    <fieldset>
-      <select name="zipfile" size="1" class="select">
+<h1>Archive Unzipper</h1>
+<p>Select .zip archive or .gz file you want to extract:</p>
+<form action="" method="POST">
+  <fieldset>
+    <select name="zipfile" size="1" class="select">
       <?php foreach ($arc->zipfiles as $zip) {
         echo "<option>$zip</option>";
-        }
-        ?>
-      </select>
-      <br />
-      <br />
-      <input type="text" name="extpath" class="form-field" placeholder="extraction_path" />
-      <label>Optional, if blank current dir will be used.</label>
-      <br/>
-      <input type="submit" name="submit" class="submit" value="Unzip Archive" />
-    </fieldset>
-  </form>
-  <p class="status">
-    Status: <?php echo $arc::$status; ?> <br />
-    Processing Time: <?php echo $time; ?> ms
-  </p>
+      }
+      ?>
+    </select>
+    <br/>
+    <br/>
+    <input type="text" name="extpath" class="form-field"
+           placeholder="extraction_path"/>
+    <label>Optional, if blank current dir will be used.</label>
+    <br/>
+    <input type="submit" name="submit" class="submit" value="Unzip Archive"/>
+  </fieldset>
+</form>
+<p class="status">
+  Status: <?php echo $arc::$status; ?> <br/>
+  Processing Time: <?php echo $time; ?> ms
+</p>
 </body>
 </html>
