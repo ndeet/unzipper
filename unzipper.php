915f64ae208bc7dc09710aac9a32dae8961d02a4
Chamuth Chamandana <c.chamandana@gmail.com>
2017-06-27 18:14:44 +0530

    CSS3 Design upgrade

--- unzipper.php
+++ unzipper.php
@@ -292,6 +292,23 @@ class Zipper {
     body {
       font-family: Arial, sans-serif;
       line-height: 150%;
+      width:50%;
+      --main-primary-color:dodgerblue;
+      background-color:#eee;
+    }
+
+    .container
+    {
+      position:absolute;
+      left:50%;
+      top:50%;
+      transform:translateX(-50%) translateY(-50%);
+    }
+
+    .status .small
+    {
+      float:right;
+      margin-right:20px;
     }
 
     label {
@@ -301,8 +318,13 @@ class Zipper {
 
     fieldset {
       border: 0;
-      background-color: #EEE;
-      margin: 10px 0 10px 0;
+      background-color: #FFF;
+      margin: 10px 0 25px 0;
+      border-radius:10px;
+      padding:10px 20px;
+      box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+
+      transition:height 1s;
     }
 
     .select {
@@ -311,25 +333,45 @@ class Zipper {
     }
 
     .status {
-      margin: 0;
-      margin-bottom: 20px;
+      margin:0;
+      float:left;
       padding: 10px;
       font-size: 80%;
-      background: #EEE;
-      border: 1px dotted #DDD;
+      background: #FFF;
+      box-shadow: 0 -2px 1px var(--main-primary-color);
+
+      position:fixed;
+      z-index:100;
+      bottom:0;
+      left:0;
+
+      width:100%;
+
+      animation: statusAnimation 1s;
+    }
+
+    @keyframes statusAnimation
+    {
+      0% { opacity: 0; transform:translateY(100%);}
+      100% {opacity: 1; transform:translateY(0%);}
+    }
+
+    .status-text
+    {
+      font-weight:bold;
     }
 
     .status--ERROR {
       background-color: red;
       color: white;
-      font-size: 120%;
+      font-size: 100%;
     }
 
     .status--SUCCESS {
-      background-color: green;
+      background-color: var(--main-primary-color);
       font-weight: bold;
       color: white;
-      font-size: 120%
+      font-size: 100%;
     }
 
     .small {
@@ -339,66 +381,165 @@ class Zipper {
 
     .version {
       font-size: 80%;
+      text-align:center;
+      margin-top:20px;
+      color:gray
     }
 
     .form-field {
       border: 1px solid #AAA;
       padding: 8px;
       width: 280px;
-    }
+      }
 
     .info {
-      margin-top: 0;
+      margin-top: 7px;
       font-size: 80%;
       color: #777;
     }
 
     .submit {
-      background-color: #378de5;
+      background-color: var(--main-primary-color);
       border: 0;
       color: #ffffff;
       font-size: 15px;
+      border-radius:5px;
       padding: 10px 24px;
       margin: 20px 0 20px 0;
       text-decoration: none;
+      font-weight:bold;
+      border:solid 2px var(--main-primary-color);
+      box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+
+      transition: background-color 1s, color 1s, box-shadow 1s;
     }
 
     .submit:hover {
-      background-color: #2c6db2;
+      color: var(--main-primary-color);
+      background-color: #fff;
       cursor: pointer;
+      box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+
+      transition: background-color 250ms, color 250ms, box-shadow 500ms
+    }
+
+    h1
+    {
+      margin-bottom:25px;
+    }
+
+    input[type="text"]:focus,
+    select:focus,
+    textarea:focus,
+    button:focus {
+        outline: none;
+        border-color:var(--main-primary-color);
+        border-width:1.5px;
+
+        transition: border 500ms;
+    }
+
+    input[type="text"],
+    select,
+    textarea
+    {
+
+      border-radius:5px;
+      margin-top:5px;
+      border-color:#AAA;
+      border-width:1.5px;
+      width: 100%; 
+      box-sizing: border-box;
+      -webkit-box-sizing:border-box;
+      -moz-box-sizing: border-box;
+
+
+      transition: border 500ms;
+    }
+
+    h1
+    {
+      text-transform:uppercase;
+      font-size:15px;
+      color:var(--main-primary-color)
+    }
+
+
+    @media screen and (max-width:1000px)
+    {
+      .container
+      {
+        width:90%;
+      }
+
+      .status .small
+      {
+        display:none;
+      }
+    }
+
+    @media screen and (max-height:768px)
+    {
+      .status
+      {
+        position:fixed;
+        width:auto;
+        bottom:20px;
+        left:20px;
+        height:20px;
+        border-radius:10px;
+
+        box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+
+        animation: statusAnimation 1s;  
+
+        border:solid 2px var(--main-primary-color);   
+
+        transition: width 500ms;
+      }
+
+      .status .small
+      {
+        display:none;
+      }
+
+     
     }
     -->
   </style>
 </head>
 <body>
 <p class="status status--<?php echo strtoupper(key($GLOBALS['status'])); ?>">
-  Status: <?php echo reset($GLOBALS['status']); ?><br/>
-  <span class="small">Processing Time: <?php echo $time; ?> seconds</span>
-</p>
-<form action="" method="POST">
-  <fieldset>
-    <h1>Archive Unzipper</h1>
-    <label for="zipfile">Select .zip or .rar archive or .gz file you want to extract:</label>
-    <select name="zipfile" size="1" class="select">
-      <?php foreach ($unzipper->zipfiles as $zip) {
-        echo "<option>$zip</option>";
-      }
-      ?>
-    </select>
-    <label for="extpath">Extraction path (optional):</label>
-    <input type="text" name="extpath" class="form-field" />
-    <p class="info">Enter extraction path without leading or trailing slashes (e.g. "mypath"). If left empty current directory will be used.</p>
-    <input type="submit" name="dounzip" class="submit" value="Unzip Archive"/>
-  </fieldset>
+    <span class="status-text">Status: <?php echo reset($GLOBALS['status']); ?></span>
+    <span class="small">Processing Time: <?= round($time, 5, PHP_ROUND_HALF_EVEN)?> seconds</span>
+  </p>
+  <div class="container">
+  
+  <form action="" method="POST">
+    <fieldset>
+      <h1>Archive Unzipper</h1>
+      <label for="zipfile">Select .zip or .rar archive or .gz file you want to extract:</label>
+      <select name="zipfile" size="1" class="select">
+        <?php foreach ($unzipper->zipfiles as $zip) {
+          echo "<option>$zip</option>";
+        }
+        ?>
+      </select>
+      <label for="extpath">Extraction path (optional):</label>
+      <input type="text" name="extpath" class="form-field" />
+      <p class="info">Enter extraction path without leading or trailing slashes (e.g. "mypath"). If left empty current directory will be used.</p>
+      <input type="submit" name="dounzip" class="submit" value="Unzip Archive"/>
+    </fieldset>
 
-  <fieldset>
-    <h1>Archive Zipper</h1>
-    <label for="zippath">Path that should be zipped (optional):</label>
-    <input type="text" name="zippath" class="form-field" />
-    <p class="info">Enter path to be zipped without leading or trailing slashes (e.g. "zippath"). If left empty current directory will be used.</p>
-    <input type="submit" name="dozip" class="submit" value="Zip Archive"/>
-  </fieldset>
-</form>
-<p class="version">Unzipper version: <?php echo VERSION; ?></p>
+    <fieldset>
+      <h1>Archive Zipper</h1>
+      <label for="zippath">Path that should be zipped (optional):</label>
+      <input type="text" name="zippath" class="form-field" />
+      <p class="info">Enter path to be zipped without leading or trailing slashes (e.g. "zippath"). If left empty current directory will be used.</p>
+      <input type="submit" name="dozip" class="submit" value="Zip Archive"/>
+    </fieldset>
+  </form>
+  <p class="version">Unzipper version: <?php echo VERSION; ?></p>
+  </div>
 </body>
 </html>
